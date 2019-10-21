48aae35787e8c0e7e37f56cd15ee9ee74443a141
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-10-21 16:06:57 +0900

    application.rbに関する追記

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module CodeColor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.action_controller.include_all_helpers = false    # コントローラに対応するものしか読み込ませたくない時は、config/application.rbに以下設定
+    config.action_controller.include_all_helpers = false
+    # コントローラに対応するものしかコードの色付けをさせたくない時は、config/application.rbに上記を設定。
+    # ただし、その場合、font-awesomeが使えなくなる
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
