0449272f31fa548c4cb65ff1f5166096ea51128a
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-10-21 20:05:52 +0900

    cofig削除(font-awesomeとの兼ね合い)

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,6 @@ module CodeColor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.action_controller.include_all_helpers = false
-    # コントローラに対応するものしかコードの色付けをさせたくない時は、config/application.rbに上記を設定。
-    # ただし、その場合、font-awesomeが使えなくなる
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
